8019140068ea54df1b02026c0d72beff8d2fc7e9
Jon Koehmstedt <Jon.Koehmstedt@gmail.com>
2013-07-31 20:41:25 -0400

    push

--- Intersection/src/queues/UnboundedQueueInterface.java
+++ Intersection/src/queues/UnboundedQueueInterface.java
@@ -1,7 +1,14 @@
 package queues;
 
-public interface UnboundedQueueInterface<T> extends QueueInterface<T> {
+public interface UnboundedQueueInterface<T> {
 	void enqueue(T element);
 	// Places element at the end of the queue.
+
+	void dequeue() throws QueueUnderflowException;
+	// Throws QueueUnderflowException if the queue is empty,
+	// otherwise removes the first element from the queue.
+
+	boolean isEmpty();
+	// returns true if this queue is empty, otherwise returns false.
 
 }
